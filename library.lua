8ae1ee18c17fc68a7062ba4552410c49cd91aa9c
Alstro20 <sb74851@gmail.com>
2020-11-23 12:49:32 -0600

    initial upload

---
+++ library.lua
@@ -0,0 +1,7 @@
+--Library updated 11/22/2020 7:47PM
+
+function whitneyIsABitch(loopAmount)
+    for i=1,loopAmount do
+        print("Whitney is a bitch")
+    end
+end
---
+++ test.lua
@@ -0,0 +1,6 @@
+require '/library'
+
+term.clear()
+
+print("How many times to loop?")
+whitneyIsABitch(read())
