22f7fda8d7e336b7ec738ca0f194b9828d5d90b1
Alstro20 <sb74851@gmail.com>
2020-11-23 13:24:59 -0600

    Added games.lua

---
+++ games.lua
@@ -0,0 +1,15 @@
+term.clear()
+print("Select a game\\n*Advanced Computer Required")
+print("1) *Doom") --6BcdRV75
+print("2) *LDRIS (Tetris)") --https://github.com/LDDestroier/CC/raw/master/ldris.lua
+local selection = io.read()
+
+term.clear()
+
+if selection=1 then
+    shell.run("pastebin run 6BcdRV75")
+elseif selection=2 then
+    shell.run("wget run https://github.com/LDDestroier/CC/raw/master/ldris.lua")
+else
+    print("Error: Invalid selection")
+end
