bca89bb80dc062694ba0d71d604bd6fb283eb781
Alstro20 <sb74851@gmail.com>
2020-11-23 13:30:05 -0600

    Added CClickers

--- games.lua
+++ games.lua
@@ -3,8 +3,10 @@ print("Select a game")
 print("*=Advanced Computer Required")
 
 
-print("1) *Doom") --6BcdRV75
+--TODO: Make print strings and game commands a table to be able to easily add new games
+print("1) *Doom [Instal Required]") --6BcdRV75
 print("2) *LDRIS (Tetris)") --https://github.com/LDDestroier/CC/raw/master/ldris.lua
+print("3) *CClickers") --zCfqpZfy
 local selection = io.read()
 
 term.clear()
@@ -13,6 +15,8 @@ if selection == "1" then
     shell.run("pastebin run 6BcdRV75")
 elseif selection == "2" then
     shell.run("wget run https://github.com/LDDestroier/CC/raw/master/ldris.lua")
+elseif election == "3" then
+    shell.run("pastebin run zCfqpZfy ")
 else
     print("Error: Invalid selection")
 end
