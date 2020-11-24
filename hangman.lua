5320f93850f78bc42bd68fdac5b2c80f749e5bba
Alstro20 <sb74851@gmail.com>
2020-11-24 00:00:54 -0600

    removed spaces because it broke things. Fix that later

--- hangman.lua
+++ hangman.lua
@@ -1,8 +1,6 @@
 require '/ComputerCraftThings/library'
 
-local validLetters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '} --Letters allowed in the game
-local validLettersPhrase
-
+--TODO: Make this into a function
 print("Enter the phrase")
 local phrase = read("*") --The phrase P1 enters
 
@@ -11,24 +9,9 @@ local phraseGuessed = false
 local guessedLetters = {}
 
 function checkIfValid(toCheck)
+    if(toCheck:match("%A")) then
+        --TODO: Change to allow spaces
 
-    --OLD CODE
-    -- --TODO: Fix this. Can't use 'in' in if statement
-    -- --Checks if phrase is valid by comparing phraseList and validLetters
-    -- for _, i in validLetters(toCheck) do
-    --     --Executed if phraseList is valid
-    --     print(table.concat(phraseList)) --debug
-    --     --TODO: Write this. Possibly have main game loop in here
-    --     return true
-    -- end
-
-    -- --Executed if phraseList is invalid
-    -- print("Error: Invalid phrase")
-    --END OLD CODE
-
-
-    --if(string.gmatch(toCheck, "%a")) then
-    if(toCheck:match("%A ")) then
         --Invalid phrase
         --print("Invalid phrase. Phrase must be alphanumeric.") --Debug
         return false
@@ -43,6 +26,7 @@ end
 
 --checkIfValid(phrase) --debug
 if checkIfValid(phrase) then
+    print("Valid phrase:")
     print(phrase)
 elseif checkIfValid(phrase) == false then
     print("Invalid phrase. Phrase must only be letters and spaces.")
