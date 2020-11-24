a65784f65dcfe461bba0defc5434ab522a3a8d77
Alstro20 <sb74851@gmail.com>
2020-11-24 15:13:27 -0600

    test

--- update.lua
+++ update.lua
@@ -1,5 +1,9 @@
+require '/ComputerCraftThings/library'
+
 term.clear()
 print("Updating from github")
 shell.run("github clone Alstro20/ComputerCraftThings")
 term.clear()
 print("Updated!")
+
+if os.getComputerType ==
