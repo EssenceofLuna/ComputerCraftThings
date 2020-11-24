8c22e297e4e5be18cf1f5513000664c8a7ca1db7
Alstro20 <sb74851@gmail.com>
2020-11-24 15:56:05 -0600

    updated turtle branch's update code

--- update.lua
+++ update.lua
@@ -1,5 +1,63 @@
+require '/ComputerCraftThings/library'
+
+--Functions to update different computer types
+--
+function updateComputer()
+    --https://github.com/Alstro20/ComputerCraftThings/tree/master
+    shell.run("github clone Alstro20/ComputerCraftThings")
+    term.clear()
+    print("Computer Updated!")
+end
+
+function updateTurtle()
+    --https://github.com/Alstro20/ComputerCraftThings/tree/turtles
+    shell.run("github clone Alstro20/ComputerCraftThings -b")
+    term.clear()
+    print("Turtle Updated!")
+end
+
+function updatePDA()
+    shell.run("github clone Alstro20/ComputerCraftThings")
+    term.clear()
+    
+    --TODO: Create a branch for PDA's
+    print("PDA's are not yet supported. Downloaded default computer branch.")
+    print("PDA Updated!")
+end
+
 term.clear()
 print("Updating from github")
-shell.run("github clone Alstro20/ComputerCraftThings")
-term.clear()
-print("Updated!")
+
+--Store the computer's type as a string
+computerType = os.getComputerType()
+
+if (#arg == 0) then
+    --If no argument provided, discover the type of computer and update accordingly
+    if computerType == 'computer' or computerType == 'advanced_computer' then
+        updateComputer()
+    elseif computerType == 'turtle' or computerType == 'advanced_turtle' then
+        updateTurtle()
+    elseif computerType == 'pda' or computerType == 'advanced_pda' then
+        updatePDA()
+    else
+        print("Error updating. Invalid computer type detected. Computer not updated.")
+        print("Computer type "..computerType.." detected")
+    end
+elseif (#arg == 1) then
+    argument = table.concat(arg)
+
+    --Update based on argument provided by user
+    if argument == 'computer' or argument == 'advanced_computer' then
+        updateComputer()
+    elseif argument == 'turtle' or argument == 'advanced_turtle' then
+        updateTurtle()
+    elseif argument == 'pda' or argument == 'advanced_pda' then
+        updatePDA()
+    else
+        print("Error updating: Invalid computer type provided by user.")
+        print("Please enter computer, turtle, of pda (all lowercase")
+        print("Or enter no argument to auto update.")
+    end
+else
+    print("Error: Too many arguments provided")
+end
