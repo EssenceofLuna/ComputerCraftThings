178d2eb257a33dc4fe40ffe1295ffac7ea3a1c3d
Alstro20 <sb74851@gmail.com>
2020-11-23 23:29:29 -0600

    stuff

--- hangman.lua
+++ hangman.lua
@@ -11,15 +11,23 @@ local guessedLetters = {}
 
 local phraseList = split(phrase, '%s') --Break phrase into list of characters
 
---TODO: Fix this. Can't use 'in' in if statement
---Checks if phrase is valid by comparing phraseList and validLetters
-if phraseList in validLetters then
-    print(table.concat(phraseList))
-else
-    print("Error: Invalid phrase")
+function checkIfValid(toCheck)
+    --TODO: Fix this. Can't use 'in' in if statement
+    --Checks if phrase is valid by comparing phraseList and validLetters
+    for toCheck in validLetters then
+        --Executed if phraseList is valid
+        print(table.concat(phraseList)) --debug
+        --TODO: Write this. Possibly have main game loop in here
+        return true
+    else
+        --Executed if phraseList is invalid
+        print("Error: Invalid phrase")
+    end
 end
 
-while phraseGuessed = false do
---     Main game loop
-    print("Guess a letter!")
-    while 
+checkIfValid(phraseList)
+
+-- while phraseGuessed = false do
+-- --Main game loop
+--     print("Guess a letter!")
+--     while 
