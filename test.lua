d310a201e97eb30f9d606234f66c59928f9ee463
Alstro20 <sb74851@gmail.com>
2020-11-24 16:00:04 -0600

    sfdgh

--- test.lua
+++ test.lua
@@ -2,6 +2,8 @@ require '/ComputerCraftThings/library'
 
 term.clear()
 
+print("Master Branch")
+
 print(os.getComputerType())
 if os.getComputerType() == 'advanced_computer' then
     print("advanced PC bitch")
