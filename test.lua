277a205a2105868fa705ea29f0d403e3f3d3b514
Alstro20 <sb74851@gmail.com>
2020-11-26 17:21:47 -0600

    rearranged test info

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ require '/ComputerCraftThings/library'
 
 term.clear()
 print("System Info")
-print("Computer ID: "..os.getComputerID())
 print("Github Branch: Master Branch")
-print("Computer type: "..os.getComputerType())
 print("Computer Label: "..os.getComputerLabel())
+print("Computer ID: "..os.getComputerID())
+print("Computer type: "..os.getComputerType())
