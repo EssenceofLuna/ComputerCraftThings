d8254124712cef42f73922e527b4d9e4c4957bd2
alex brown <sb74851@gmail.com>
2020-11-26 13:32:07 -0600

    Added getComputerLabel()

--- test.lua
+++ test.lua
@@ -2,6 +2,7 @@ require '/ComputerCraftThings/library'
 
 term.clear()
 print("System Info")
-print("Computer ID: "..os.getComputerID)
+print("Computer ID: "..os.getComputerID())
 print("Github Branch: Master Branch")
 print("Computer type: "..os.getComputerType())
+print("Computer Label: "..os.getComputerLabel())
