8259012670455cb84ff1ea8ee04e7865efa942e9
Alstro20 <sb74851@gmail.com>
2020-11-27 11:15:52 -0600

    testing gps things

--- test.lua
+++ test.lua
@@ -1,13 +1,25 @@
 require '/ComputerCraftThings/library'
 
-local x, y, z = gps.locate(5)
 
-term.clear()
-print("System Info")
-print("Github Branch: Turtles")
-print("Computer Label: "..os.getComputerLabel())
-print("Computer ID: "..os.getComputerID())
-print("Computer type: "..os.getComputerType())
-print("Turtle Fuel: "..turtle.getFuelLevel().."/"..turtle.getFuelLimit())
-print("OS Version: "..os.version())
-print("Coords: "..x.." "..y.." "..z)
+
+print("Man I am so lost right now!")
+local x, y, z = gps.locate(5)
+if not x then --If gps.locate didn't work, it won't return anything. So check to see if it did.
+  print("Failed to get my location!")
+else
+  print("I am at (" .. x .. ", " .. y .. ", " .. z .. ")") --This prints 'I am at (1, 2, 3)' or whatever your coordinates are
+end
+
+
+
+-- local x, y, z = gps.locate(5)
+
+-- term.clear()
+-- print("System Info")
+-- print("Github Branch: Turtles")
+-- print("Computer Label: "..os.getComputerLabel())
+-- print("Computer ID: "..os.getComputerID())
+-- print("Computer type: "..os.getComputerType())
+-- print("Turtle Fuel: "..turtle.getFuelLevel().."/"..turtle.getFuelLimit())
+-- print("OS Version: "..os.version())
+-- print("Coords: "..x..", "..y..", "..z)
