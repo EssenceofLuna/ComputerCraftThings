71e938ffb8cbf524c8df0f4eb170f6b7c0695a60
Alstro20 <sb74851@gmail.com>
2020-11-28 10:56:05 -0600

    stuff

--- test.lua
+++ test.lua
@@ -1,18 +1,23 @@
 require '/ComputerCraftThings/library'
 
 
-local x, y, z = gps.locate(5)
+-- local x, y, z = gps.locate(5)
 
-term.clear()
-print("System Info")
-print("Github Branch: Turtles")
-print("Computer Label: "..os.getComputerLabel())
-print("Computer ID: "..os.getComputerID())
-print("Computer type: "..os.getComputerType())
-print("Turtle Fuel: "..turtle.getFuelLevel().."/"..turtle.getFuelLimit())
-print("OS Version: "..os.version())
-if x ~= nil then
-    print("Coords: "..x..", "..y..", "..z)
-else
-    print("Error: Corrdinates not obtained.")
+-- term.clear()
+-- print("System Info")
+-- print("Github Branch: Turtles")
+-- print("Computer Label: "..os.getComputerLabel())
+-- print("Computer ID: "..os.getComputerID())
+-- print("Computer type: "..os.getComputerType())
+-- print("Turtle Fuel: "..turtle.getFuelLevel().."/"..turtle.getFuelLimit())
+-- print("OS Version: "..os.version())
+-- if x ~= nil then
+--     print("Coords: "..x..", "..y..", "..z)
+-- else
+--     print("Error: Corrdinates not obtained.")
+-- end
+
+print(turtle.compare())
+if turtle.compare() == "minecraft:cobblestone" then
+    turtle.dig()
 end
