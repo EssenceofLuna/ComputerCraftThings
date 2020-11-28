ab0ac2ae5144b10e98c77061e37d780eb4764c54
Alstro20 <sb74851@gmail.com>
2020-11-28 13:28:44 -0600

    rows and columns instead of i

--- excavate.lua
+++ excavate.lua
@@ -3,38 +3,37 @@ require '/ComputerCraftThings/turtleLibrary'
 
 function excavate(depth, width)
     manageInventory()
-    for i=1,width do
-
-        if i == 2 then
-            depth = depth - 1
-        end
-
-        for i=1,depth do
+    for columns=1,width do
+        for rows=1,depth do
             --manage inventory every 30 blocks
-            if i % 30 == 0 then
+            if rows % 30 == 0 then
                 manageInventory()
             end
             turtle.digForward()
         end
-
+        
         turtle.turnRight()
         turtle.turnRight()
         turtle.moveForward(depth)
-
+        
         manageInventory()
         
-        if i ~= width then
+        if columns ~= width then
             --Turn to next row
             turtle.turnLeft()
             turtle.digForward()
             turtle.turnLeft()
         end
     end
-
+    
     --Finished excavating. Returning to start
     turtle.turnRight()
     turtle.moveForward(width-1)
     turtle.turnRight()
+
+    if columns == 1 then
+        depth = depth - 1
+    end
 end
 
 excavate(5, 5)
