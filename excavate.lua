aa837358b2fb7a2cd9d31982d757380e1531afa4
Alstro20 <sb74851@gmail.com>
2020-11-28 13:14:21 -0600

    added inventory management

--- excavate.lua
+++ excavate.lua
@@ -4,11 +4,19 @@ require '/ComputerCraftThings/turtleLibrary'
 function excavate(depth, width)
     manageInventory()
     for i=1,width do
-        turtle.digForward(depth)
+        -- turtle.digForward(depth)
+        for i=1,depth do
+            --manage inventory every 30 blocks
+            if i % 30 == 0 then
+                manageInventory()
+            end
+            turtle.digForward()
+        end
         turtle.turnRight()
         turtle.turnRight()
         turtle.moveForward(depth)
 
+        --Turn to next row
         turtle.turnLeft()
         turtle.digForward()
         turtle.turnLeft()
