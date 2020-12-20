921b86001173abd3ddf653ffacbd3fd274794c1d
Alstro20 <sb74851@gmail.com>
2020-12-20 17:49:47 -0600

    -

--- reactorcontroller.lua
+++ reactorcontroller.lua
@@ -8,7 +8,7 @@ turbineCount = 0
 for turbineIndex = 1,1000 do --TODO: Maybe change to while loop idk
     --turbineIndex is the int value of the current check, starting at 1
     --turbineStr is the string for the peripheral name
-    turbineStr = 'BigReactorsTurbine_'..turbineIndex
+    turbineStr = 'BigReactors-Turbine_'..turbineIndex
     print('DEBUG: Checking '..turbineIndex..' named '..turbineStr) --DEBUG
     if peripheral.isPresent(turbineStr) then
         print('DEBUG: '..turbineStr..' was found. Adding to index..') --DEBUG
