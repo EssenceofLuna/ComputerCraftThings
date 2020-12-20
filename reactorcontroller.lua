f48da3e7d389f3c12f7766e28a7803c85390c2db
Alstro20 <sb74851@gmail.com>
2020-12-20 17:46:04 -0600

    added turbine indexing

---
+++ reactorcontroller.lua
@@ -0,0 +1,23 @@
+require '/ComputerCraftThings/library'
+
+--TODO: Write pocket computer program to check on reactor stuff
+
+turbines = {}
+turbineCount = 0
+
+for turbineIndex = 1,1000 do --TODO: Maybe change to while loop idk
+    --turbineIndex is the int value of the current check, starting at 1
+    --turbineStr is the string for the peripheral name
+    turbineStr = 'BigReactorsTurbine_'..turbineIndex
+    if peripherl.isPresent(turbineStr) then
+        table.insert(turbines, turbineStr)
+    else
+        --Once a turbine is not found, set the turbineCount and break the loop
+        turbineCount = turbineIndex-1
+        break
+    end
+end
+
+--DEBUG
+print('DEBUG: turbine count: '..tostring(turbineCount))
+print('DEBUG: turbines: '..table.concat(turbines))
