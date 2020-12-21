e5e245de15b33229f5d50162c06c1c6ffa798b87
Alstro20 <sb74851@gmail.com>
2020-12-20 22:08:42 -0600

    fsdfsdfsdfdsfsdf

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 require '/ComputerCraftThings/library'
-
+local surface = dofile('surface')
 
 -- term.clear()
 -- print("System Info")
@@ -15,24 +15,7 @@ require '/ComputerCraftThings/library'
 -- print('Turbine Type: '..peripheral.getType(turbineStr))
 -- print('Turbine Speed: '..turbine.getRotorSpeed())
 
-function drawSquare(xmin,xmax,ymin,ymax,text)
-    numB = ymax - ymin
-    numA = xmax - xmin
-    
-    for i = 1,numB do
-        term.setCursorPos(xmin,(ymin+i))
-        print("|" .. string.rep(" ",numA-2) .. "|")
-    end
-    term.setCursorPos(xmin,ymin)
-    print(string.rep("=",numA))
-    term.setCursorPos(xmin,ymax)
-    print(string.rep("=",numA))
-    term.setCursorPos((numA/2) + xmin - (string.len(text)/2), ymin + (numB/2))
-    print(text)
-    term.setCursorPos(1,ymax+1)
-end
 
-drawSquare(5,50,5,25,'TEST BITCH')
 
 -- while true do
 --     event, side, xPos, yPos = os.pullEvent("monitor_touch")
@@ -40,3 +23,14 @@ drawSquare(5,50,5,25,'TEST BITCH')
 --         "X: "..tostring(xPos)..", "..
 --         "Y: "..tostring(yPos))
 -- end
+
+term.clear()
+
+surf:drawRect(5, 5, 25, 10)
+
+while true do
+    event, side, xPos, yPos = os.pullEvent("monitor_touch")
+    if xPos >= 5 and xPos <= 25 and yPos >= 5 and yPos <= 10 then
+        print("Button Clicked!")
+    end
+end
