4175b849c8f7b2c7a282049bbe84fcafd0891bf6
Alstro20 <sb74851@gmail.com>
2020-12-20 20:20:59 -0600

    test

--- test.lua
+++ test.lua
@@ -1,4 +1,6 @@
 require '/ComputerCraftThings/library'
+os.loadAPI("button")
+button.setMonitor(top)
 
 -- term.clear()
 -- print("System Info")
@@ -7,9 +9,14 @@ require '/ComputerCraftThings/library'
 -- print("Computer ID: "..os.getComputerID())
 -- print("Computer type: "..os.getComputerType())
 
-turbineStr = 'BigReactors-Turbine_1'
-turbine = peripheral.wrap(turbineStr)
+-- turbineStr = 'BigReactors-Turbine_1'
+-- turbine = peripheral.wrap(turbineStr)
 
-print('Turbine Connected: '..tostring(peripheral.isPresent(turbineStr)))
-print('Turbine Type: '..peripheral.getType(turbineStr))
-print('Turbine Speed: '..turbine.getRotorSpeed())
+-- print('Turbine Connected: '..tostring(peripheral.isPresent(turbineStr)))
+-- print('Turbine Type: '..peripheral.getType(turbineStr))
+-- print('Turbine Speed: '..turbine.getRotorSpeed())
+
+testButton = button.create("Test Button")
+testButton.setPos(1,1)
+testButton.onClick(function() print("Button Clicked") end)
+while true do button.await(myButton) end
