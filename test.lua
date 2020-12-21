5c5cd375d40e300a9d3d6842cf49d8727a22a7c0
Alstro20 <sb74851@gmail.com>
2020-12-20 22:03:19 -0600

    moved stuff

--- test.lua
+++ test.lua
@@ -15,3 +15,28 @@ require '/ComputerCraftThings/library'
 -- print('Turbine Type: '..peripheral.getType(turbineStr))
 -- print('Turbine Speed: '..turbine.getRotorSpeed())
 
+function drawSquare(xmin,xmax,ymin,ymax,text)
+    numB = ymax - ymin
+    numA = xmax - xmin
+    
+    for i = 1,numB do
+        term.setCursorPos(xmin,(ymin+i))
+        print("|" .. string.rep(" ",numA-2) .. "|")
+    end
+    term.setCursorPos(xmin,ymin)
+    print(string.rep("=",numA))
+    term.setCursorPos(xmin,ymax)
+    print(string.rep("=",numA))
+    term.setCursorPos((numA/2) + xmin - (string.len(text)/2), ymin + (numB/2))
+    print(text)
+    term.setCursorPos(1,ymax+1)
+end
+
+drawSquare(5,10,5,25,'TEST BITCH')
+
+-- while true do
+--     event, side, xPos, yPos = os.pullEvent("monitor_touch")
+--     print(event.."=> Side: "..tostring(side)..", "..
+--         "X: "..tostring(xPos)..", "..
+--         "Y: "..tostring(yPos))
+-- end
