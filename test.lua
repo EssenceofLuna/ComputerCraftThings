8d7e51f6205eed6af6b5bbd6f3777f12f53f1e29
Alstro20 <sb74851@gmail.com>
2020-12-20 21:23:19 -0600

    test

--- test.lua
+++ test.lua
@@ -1,5 +1,4 @@
 require '/ComputerCraftThings/library'
-require '/ComputerCraftThings/button'
 local surface = dofile("surface") --load Surface (Graphics API)
 
 -- term.clear()
@@ -16,14 +15,4 @@ local surface = dofile("surface") --load Surface (Graphics API)
 -- print('Turbine Type: '..peripheral.getType(turbineStr))
 -- print('Turbine Speed: '..turbine.getRotorSpeed())
 
--- Creates a new surface with a blue background with a size of 51x19 pixels.
-local surf = surface.create(51, 19, colors.blue)
-
--- Draws the string "Hello, world!" at position (2,2).
-surf:drawString("Hello, world!", 2, 2)
-
--- Outputs the surface to the screen.
-surf:output()
-
--- Waits for a mouse click.
-os.pullEvent("mouse_click")
+print('CC Version: '..os.version())
