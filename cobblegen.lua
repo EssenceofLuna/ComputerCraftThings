63e71b365d8ce78bd7762dcbc2fb8745d382a8ef
Alstro20 <sb74851@gmail.com>
2021-11-01 17:05:18 -0500

    then, not do

--- cobblegen.lua
+++ cobblegen.lua
@@ -9,8 +9,8 @@ while true do
     for i=1,63 do
         turtle.dig()
     
-    if CHEST_SIDE == "left" do
+    if CHEST_SIDE == "left" then
         turtle.turnLeft()
-        if turtle.detect() == "minecraft:chest" do
+        if turtle.detect() == "minecraft:chest" then
             getItemIndex("minecraft:cobblestone") 
             turtle.drop()
