6c907ac137df6935bfcd8329eebb687ab101f094
Alstro20 <sb74851@gmail.com>
2021-11-01 17:05:54 -0500

    sdfsdfdsf

--- cobblegen.lua
+++ cobblegen.lua
@@ -8,9 +8,11 @@ CHEST_SIDE = "left"
 while true do
     for i=1,63 do
         turtle.dig()
+    end
     
     if CHEST_SIDE == "left" then
         turtle.turnLeft()
         if turtle.detect() == "minecraft:chest" then
             getItemIndex("minecraft:cobblestone") 
             turtle.drop()
+    end
