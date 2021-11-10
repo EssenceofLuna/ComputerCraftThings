f766f570fc1b0eb68db7e2a0971fbd0cb7fc8db4
Alstro20 <sb74851@gmail.com>
2021-11-09 20:20:11 -0600

    sdsdfsdf

--- TheButton.lua
+++ TheButton.lua
@@ -173,7 +173,7 @@ function resetButton()
     --animation
     --NEEDS TESTING!!!
     term.setCursorPos(8,5)
-    textutils.slowWrite("BUTTON PRESSED!!", 3)
+    textutils.slowWrite("BUTTON PRESSED!!", 8)
     
 
     term.setCursorPos(8,5)
